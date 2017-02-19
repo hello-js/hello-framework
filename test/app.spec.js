6799d3c51a44071f48fd5b92f0c8c579f9e34ed7
Matt Venables <mattvenables@gmail.com>
2017-02-18 21:25:03 -0500

    Add basic App class using koa-plus

---
+++ test/app.spec.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const App = require('../lib/app')
+const request = require('supertest')
+
+describe('App', function () {
+  describe('constructor', function () {
+    it('creates a new app instance', function () {
+      let app = new App()
+
+      return request(app.listen())
+        .get('/')
+        .expect(404)
+    })
+  })
+})
