e84de969b7586308fb45f381ddbb26c376b7939f
Matt Venables <mattvenables@gmail.com>
2017-05-21 21:20:39 -0400

    Remove X-Client-Request-Id header

--- test/unit/middleware/request-id.spec.js
+++ test/unit/middleware/request-id.spec.js
@@ -14,16 +14,4 @@ describe('request-id middleware', function () {
       .get('/')
       .expect('X-Request-Id', uuid.regexp)
   })
-
-  it('adds the `X-Client-Request-Id` header to the response if provided by the client', function () {
-    let clientId = 'THE_CLIENT_ID'
-    let app = new Koa()
-    app.use(middleware)
-
-    return request(app.listen())
-      .get('/')
-      .set('X-Request-Id', clientId)
-      .expect('X-Request-Id', uuid.regexp)
-      .expect('X-Client-Request-Id', clientId)
-  })
 })
