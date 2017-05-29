05c03cc8223b215e9954a21843759af120d0e92d
Matt Venables <mattvenables@gmail.com>
2017-05-29 10:11:18 -0400

    Add default config, public file

--- cli/generators/templates/app/config/environments/default.js
+++ cli/generators/templates/app/config/environments/default.js
@@ -1,5 +1,33 @@
 'use strict'
 
+const path = require('path')
+
 module.exports = {
-  port: process.env.PORT || 3000
+  db: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, '..', '..', 'db', 'migrations'),
+      tableName: 'hello_migrations'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
+
+  json: {
+    pretty: false,
+    spaces: 2
+  },
+
+  logging: {
+    format: 'common'
+  },
+
+  port: process.env.PORT || 3000,
+
+  public: {
+    root: path.join(__dirname, '..', '..', 'public')
+  }
 }
--- cli/generators/templates/app/config/environments/development.js
+++ cli/generators/templates/app/config/environments/development.js
@@ -1,5 +1,21 @@
 'use strict'
 
 module.exports = {
+  db: {
+    connection: {
+      host: '127.0.0.1',
+      user: 'hello_template',
+      database: 'hello_templates_development'
+    },
+    debug: true
+  },
 
+  json: {
+    pretty: true,
+    spaces: 2
+  },
+
+  logging: {
+    format: 'dev'
+  }
 }
